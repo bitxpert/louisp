56bd0595d3aeace907ba8ba034d72ffa9508cb97
bitxpert <bitxpert@gmail.com>
2014-10-09 19:18:32 +0500

    assets precompile

---
+++ public/assets/entertainments_of_DAP-01525394cdcdaff79e0ba6f60360db76.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+   
+
+
+   var otable =  $('#entertainments').dataTable( {
+        sPaginationType: "full_numbers",
+        bJQueryUI: true,
+        processing: true,
+        serverSide: true,
+        sAjaxSource: $('#entertainments').data('source')
+    } );
+
+
+
+   var row_id
+   $('#entertainments tbody').on('keyup', 'tr', function () {
+        row_id = $('td', this).eq(0).text();
+    } );
+   var update_url = "/entertainments/dumy_update"
+   otable.makeEditable({
+        sUpdateURL: update_url,
+        aoColumns: [
+                        null,
+                        {   
+                            placeholder: '',
+                            method: 'GET',
+                        },
+                        {
+                            placeholder: '',
+                            method: 'GET'
+                        },
+                        {
+                            placeholder: '',
+                            method: 'GET'
+                        },
+                        {
+                            placeholder: '',
+                            method: 'GET'
+                        }
+                ],
+        fnOnEditing: function(input)
+                {   
+                    //$("#trace").append("Updating cell with value " + input.val());
+                    /*otable.$('tr').keyup( function () {
+                    aData = otable.fnGetData(this); // get datarow 
+                    console.log(aData)
+                    });*/
+
+                    update_url = "http://localhost:3000/entertainments/"+row_id
+                    try {
+                        x = $('#'+row_id+' td')
+                            for (i = 0; i < x.length; i++) {
+                                if(x[i].childNodes[0].childNodes[0])
+                                    {
+                                    x[i].innerHTML = input.val()
+                                    }   
+                                    
+                            }    
+                    }
+                    catch(err) {
+                        console.log("asd")
+                    }
+                    
+
+                    
+                     /*otable.$('tr').keyup( function () {
+                    aData = otable.fnGetData(this); // get datarow 
+                    console.log(aData)
+                    });*/
+                    
+                      //console.log(otable)  
+                    /*var aPos = otable.fnGetPosition($("#11")[0]);    
+                    console.log(document.getElementsByTagName("input"))
+                    console.log(otable.fnGetData(aPos));
+                    console.log(input);*/
+                    // update_url = "http://localhost:3000/entertainments/"+row_id
+                    
+
+                    // $.ajax({
+                    //     url: "/entertainments/"+row_id,
+                    //     type: "put",
+                    //     data: {
+                    //         'entertainment[id]' : x[0].innerHTML,
+                    //         'entertainment[name]' : x[1].innerHTML,
+                    //         'entertainment[state_or_province]' : x[2].innerHTML,
+                    //         'entertainment[url]' : x[3].innerHTML,
+                    //         'entertainment[phone_number]' : x[4].innerHTML
+                    //       }
+                    // });
+
+                      
+                   otable.fnDraw();
+                }    
+    });
+
+    
+   
+} );
